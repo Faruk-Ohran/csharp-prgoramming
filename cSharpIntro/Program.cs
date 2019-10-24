b814376e40898ce831f0b493357d5d2c0218ea0a
Denis Mušić <37282231+denis-music@users.noreply.github.com>
2019-10-24 12:42:55 +0200

    Add files via upload

---
+++ cSharpIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using cSharpIntro.P1;
+using cSharpIntro.P2;
+using System;
+using System.Collections.Generic;
+
+namespace cSharpIntro
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            MainP2.Run();
+            Console.ReadKey();
+        }
+    }
+
+
+}
